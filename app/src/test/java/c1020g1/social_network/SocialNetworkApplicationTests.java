9c910293e16b920a5ee76ec321fd0c6c5fb06760
son <son@codegym.vn>
2023-03-01 14:10:26 +0700

    Revert "specific test classes"
    This reverts commit 937f07efb4affd94733dd39124443f9d8dba43b8.

--- app/src/test/java/c1020g1/social_network/SocialNetworkApplicationTests.java
+++ app/src/test/java/c1020g1/social_network/SocialNetworkApplicationTests.java
@@ -3,7 +3,7 @@ package c1020g1.social_network;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = SocialNetworkApplication.class)
+@SpringBootTest
 class SocialNetworkApplicationTests {
 
     @Test
